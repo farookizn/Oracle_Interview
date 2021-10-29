4b7259244c16fd316157365a82158e069d60aa17
Farook Ali <92211338+farookizn@users.noreply.github.com>
2021-10-29 22:27:02 +0100

    Add files via upload

---
+++ Challenge1/modules/network/output.tf
@@ -0,0 +1,28 @@
+output "bastion_subnet" {
+  value = "${oci_core_subnet.bastion.id}"
+}
+
+output "server1_subnet" {
+  value = "${oci_core_subnet.private1.id}"
+}
+
+output "server2_subnet" {
+  value = "${oci_core_subnet.private2.id}"
+}
+
+output "pri_lb_subnet" {
+  value = "${oci_core_subnet.public1.id}"
+}
+
+output "sby_lb_subnet" {
+  value = "${oci_core_subnet.public2.id}"
+}
+
+output "ad1" {
+  value = "${oci_core_subnet.private1.availability_domain}"
+}
+
+output "ad2" {
+  value = "${oci_core_subnet.private2.availability_domain}"
+}
+
